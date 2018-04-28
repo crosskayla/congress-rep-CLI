c9ffb102a87e33ac7f1671a84ed092c3a4c19d60
Kayla Cross <kac288@georgetown.edu>
2018-04-28 22:03:14 +0000

    building scraper class and setting up environment

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,9 @@
 #command line project start
-class CLI
+class Scraper
+
+  def get_page
+    n = Nokogiri::HTML(open("https://www.senate.gov/general/contact_information/senators_cfm.cfm"))
+    puts n.size
+  end
+
 end
