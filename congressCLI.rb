f4892706290bcbb625d57d060b42417cab5f5bcf
Kayla Cross <kac288@georgetown.edu>
2018-04-28 22:55:36 +0000

    senator class work

--- congressCLI.rb
+++ congressCLI.rb
@@ -2,3 +2,4 @@ require_relative '../my_new_directory/config/environment'
 
 s = Scraper.new
 s.scrape_senators
+Senator.print_names
--- senator.rb
+++ senator.rb
@@ -2,16 +2,34 @@ class Senator
 
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def self.new_from_website(s)
+    s.shift
     els = s.css('a')
     sen_names = els.collect {|e| e.text.strip}
     sen_names.reject!{|name| name.include?("www.") || name.empty?}
-    sen_names.each_with_index do |name, i|
-      puts "#{i+1}- #{name}"
+    sen_names.each do |name|
+      n = name.split(', ')
+      new(n[1], n[0])
+      n.clear
     end
+
+    binding.pry
   end
 
-  def initialize()
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_names
+    all.each_with_index {|obj, i| puts "#{i+1}. #{obj.first_name} #{obj.last_name}"}
   end
 
 end
