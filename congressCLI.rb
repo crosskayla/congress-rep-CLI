c838d024dc890696b57f95b533e1b0b417f35d5c
Kayla Cross <kac288@georgetown.edu>
2018-04-28 22:32:24 +0000

    list of senators

--- congressCLI.rb
+++ congressCLI.rb
@@ -1,1 +1,4 @@
-require_relative '../config/environment'
+require_relative '../my_new_directory/config/environment'
+
+s = Scraper.new
+s.scrape_senators
--- scraper.rb
+++ scraper.rb
@@ -3,7 +3,15 @@ class Scraper
 
   def get_page
     n = Nokogiri::HTML(open("https://www.senate.gov/general/contact_information/senators_cfm.cfm"))
-    puts n.size
+  end
+
+  def scrape_senators
+    els = get_page.css('div.contenttext a')
+    sen_names = els.collect {|e| e.text.strip}
+    sen_names.reject!{|name| name.include?("www.") || name.empty?}
+    sen_names.each_with_index do |name, i|
+      puts "#{i+1}- #{name}"
+    end
   end
 
 end
